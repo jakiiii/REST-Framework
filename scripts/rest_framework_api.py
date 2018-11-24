f1fd6d08eefb99c1b42aa74260dadd22b4b20269
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-11-24 11:05:59 +0600

    permission test with python request

--- scripts/rest_framework_api.py
+++ scripts/rest_framework_api.py
@@ -7,6 +7,24 @@ ENDPOINT = 'http://127.0.0.1:8000/api/status/'
 image_path = os.path.join(os.getcwd(), 'drf.png')
 
 
+get_endpoint = ENDPOINT + str(13)
+post_data = json.dumps({'content': 'some random content for test'})
+
+
+r = requests.get(get_endpoint)
+print(r.text)
+
+r2 = requests.get(get_endpoint)
+print(r2.status_code)
+
+post_headers = {
+    'content-type': 'application/json'
+}
+
+post_response = requests.post(ENDPOINT, data=post_data, headers=post_headers)
+print(post_response.text)
+
+
 def do_img(method='get', data={},  is_json=True, img_path=None):
     headers = {}
     if is_json:
